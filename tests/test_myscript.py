71f327516b128100a7a27d9eefb1b8fc401de5dc
offline-dev-training <offlinedevtraining@gmail.com>
2026-04-15 02:28:55 +0200

    Forcing the CI to fail by lowering coverage rate

--- tests/test_myscript.py
+++ tests/test_myscript.py
@@ -1,6 +1,6 @@
 import unittest
 
-from myscript import goodbye, hello
+from myscript import hello
 
 
 class TestMyScript(unittest.TestCase):
@@ -9,7 +9,7 @@ class TestMyScript(unittest.TestCase):
         expected = "hello world!"
         self.assertEqual(actual, expected)
 
-    def test_goodbye(self):
-        actual = goodbye()
-        expected = "goodbye world!"
-        self.assertEqual(actual, expected)
+    # def test_goodbye(self):
+    #     actual = goodbye()
+    #     expected = "goodbye world!"
+    #     self.assertEqual(actual, expected)
