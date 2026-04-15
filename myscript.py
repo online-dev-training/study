2d79fcfadf2fd40b5be11c34e2103bf8ca82ba96
offline-dev-training <offlinedevtraining@gmail.com>
2026-04-15 02:02:48 +0200

    Added study script

---
+++ myscript.py
@@ -0,0 +1,11 @@
+"""Study Script."""
+
+
+def hello():
+    """Returning a hello world string."""
+    return "hello world!"
+
+
+def goodbye():
+    """Returning a goodbye world string."""
+    return "goodbye world!"
