842d5ae43a50f56587a7d6e632f5d0892ebf5fe5
offline-dev-training <offlinedevtraining@gmail.com>
2026-04-15 02:01:58 +0200

    Added unit tests

---
+++ tests/test_myscript.py
@@ -0,0 +1,15 @@
+import unittest
+
+from myscript import goodbye, hello
+
+
+class TestMyScript(unittest.TestCase):
+    def test_hello(self):
+        actual = hello()
+        expected = "hello world!"
+        self.assertEqual(actual, expected)
+
+    def test_goodbye(self):
+        actual = goodbye()
+        expected = "goodbye world!"
+        self.assertEqual(actual, expected)
